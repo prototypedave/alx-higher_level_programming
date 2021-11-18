6104ae6ab1f5f49a8fcaa0e95ade29b62a72ec42
prototypedave <popupgfj@gmail.com>
2021-11-18 08:49:26 -0800

    args function

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def add(a, b):
+    """My addittion function
+
+    Args:
+    a: first interger
+    b: second interger
+
+    Returns:
+    The return value. a + b
+    """
+    return (a + b)
