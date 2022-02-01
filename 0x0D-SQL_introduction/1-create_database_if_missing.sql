69b0638035e75a752a0b2b0ec2dacaf8a7465d31
prototypedave <popupgfj@gmail.com>
2022-02-01 07:52:20 -0800

    Deletes an existing database on mysql server

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,3 @@
+-- creates hbtb_0c_0 in your MySQL server
+
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,4 @@
+-- Deletes a database if present on your MySQL server
+-- If database is missing it still excutes
+
+DROP DATABASE IF EXISTS hbtn_0c_0;
