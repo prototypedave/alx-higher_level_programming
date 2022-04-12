d37f5de72293865b43885d55ad8454ffd1524216
prototypedave <popupgfj@gmail.com>
2022-04-12 17:44:54 +0000

    js scrapping

---
+++ 0x14-javascript-web_scrapping/1-writeme.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const fs = require('fs');
+const file = process.argv[2];
+const data = process.argv[3];
+fs.writeFile(file, data, 'utf-8', function (err) {
+  if (err) {
+    console.log(err)
+  }
+});
