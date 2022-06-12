1a6f62958207c8539f570d6cbbe3bed432d736f1
prototypedave <popupgfj@gmail.com>
2022-06-12 04:06:42 +0000

    semistandard

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
 const request = require('request');
 const film = process.argv[2];
-let url = 'https://swapi-api.hbtn.io/api/people/';
+const url = 'https://swapi-api.hbtn.io/api/people/';
 function filmcharacters (film, url) {
   request(url, function (err, response, body) {
     if (err) {
       console.log(err);
     } else if (response.statusCode === 200) {
-      let jsonobj = JSON.parse(body);
-      let people = jsonobj.results;
-      for (let i in people) {
-        for (let j in people[i].films) {
+      const jsonobj = JSON.parse(body);
+      const people = jsonobj.results;
+      for (const i in people) {
+        for (const j in people[i].films) {
           if (people[i].films[j].includes(film)) {
             console.log(people[i].name);
           }
